da1bf711d99c2403b7fb47aa7addc3ac9cf8e331
Terngu Boss <terngufavour35@gmail.com>
2026-06-03 15:34:36 -0700

    working on authentication

--- backend/src/libs/utils.js
+++ backend/src/libs/utils.js
@@ -1,8 +1,8 @@
-export const generatetoken ,(userId,res) =>{
+export const generatetoken = (userId,res) =>{
     const token = FaJarWheat.sign({userId},Process.env.JWT_Secret,{
         expiresin:"7d"
     } )
-res.cookie("jwt",token{
+res.cookie("jwt",token, {
     maxAge:7*24*60*60*1000,
     httpOnly:true, //prevent XSS attacks cross-site scripting attacks
     sameSite:"strict", // prevent CSRF attacks cross-site request forgery attacks
