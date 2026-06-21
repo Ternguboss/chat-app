d90e3b3ba5dcdc270a9abd7afe463da3542e9e04
Terngu Boss <terngufavour35@gmail.com>
2026-06-21 13:08:14 -0700

    added basic file structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,37 @@
-function App() {
+import React from 'react'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
+import { Routes,Route } from 'react-router-dom'
+import { axiosInstance } from './lib/axios'
+import { useAuthStore } from './store/useAuthStore'
+
+const App = () => {
+
+  const{authUser,checkAuth} = useAuthStore();
+
+  useEffect(()=>{
+    checkAuth()
+  },[checkAuth])
+
+  console.log({authUser})
+
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-slate-100">
-      <h1 className="text-3xl font-bold underline text-blue-600">
-        Tailwind v4 is working!
-      </h1>
-    </div>  
-  );
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/signup' element={<SignUpPage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/settings' element={<SettingsPage/>} />
+        <Route path='/profile' element={<ProfilePage/>} />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set)=>({
+    authUser: null,
+    isSigningUP: false,
+    isLogingUP: false,
+    isUpdatingProfile: true,
+
+    isCheckingAuth: true,
+
+    checkAuth: async () => {
+       try {
+        const res = await axiosInstance.get("/auth/check")
+        set({authUser:res.data}) 
+       } catch (error) {
+        console.log("error in checkAuth: ",error)
+        set({authUser:null}) 
+
+       } finally{
+        set({isCheckingAuth: false})
+       }
+    },
+}))
